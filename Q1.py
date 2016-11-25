45840db637963e0d9e2d1620e9a9b3cf8b361a98
milu-buet <milu.buet@gmail.com>
2016-11-25 16:27:33 -0600

    ok

--- Q1.py
+++ Q1.py
@@ -1,4 +1,17 @@
 #Author: Milu
+from graph import DGraph
+import random
 
 
-print('hello')
+def create_complete_weighted_undirected_graph(n, weight_limit):
+    g = DGraph()
+    for i in range(n):
+        for j in range(i,n):
+            random_weight = random.randint(0,weight_limit)
+            g.add(i,j,random_weight)
+            g.add(j,i,random_weight)
+    return g
+
+
+g = create_complete_weighted_undirected_graph(5,100)
+print(g.__dict__)
