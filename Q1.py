d4f403cc4728e3339c586bd7a187a00284ce2b09
milu-buet <milu.buet@gmail.com>
2016-11-28 23:36:15 -0600

    a

--- Q1.py
+++ Q1.py
@@ -7,61 +7,45 @@ def create_complete_weighted_undirected_graph(n, weight_limit):
     g = DGraph()
     for i in range(n):
         for j in range(i+1,n):
-            #print (i,j)
             random_weight = random.randint(1,weight_limit)
             g.add(i,j,random_weight)
             g.add(j,i,random_weight)
     return g
 
-
-
 def getMinTour(g,n,i):
-    global min_weight,min_path,path
-    current_weight = get_path_weight(g,i,path)
-    if promising(i,current_weight):
-        if i == n:
+    global min_weight,min_tour,config
+    current_weight = get_tour_weight(g,i,config)
+    if promising(i,current_weight,min_weight):
+        if i == n-1:
             min_weight = current_weight
-            min_path = path[0:]
-            #print(path,current_weight)
-        elif i == n-1:
-            path[i+1] = path[0]
-            getMinTour(g,n,i+1)
+            min_tour = config[0:] + [config[0]]
         else:
             for j in range(n):
-                if j not in path[0:i+1]:
-                    path[i+1] = j
+                if j not in config[0:i+1]:
+                    config[i+1] = j
                     getMinTour(g,n,i+1)
 
 
-def get_path_weight(g,j,path):
-	total_weight = 0
-	for i in range(j):
-		if g[path[i],path[i+1]]:
-			total_weight+=g[path[i],path[i+1]]
-		else:
-			print(g.Edges,path[i],path[i+1])
-	return total_weight
+def get_tour_weight(g,j,config):
+    if j <= 0:
+        return None
+    total_weight = 0
+    for i in range(j):
+        total_weight+=g[config[i],config[i+1]]
+    total_weight+= g[config[j],config[0]]
+    return total_weight
 
-def promising(i,current_weight):
-
-    # #print(min_path)
+def promising(i,current_weight,min_weight):
     if min_weight is None or current_weight < min_weight:
         return True
     return False
 
-
-
-
-for i in range(3,10):
+for i in range(3,12):
     n = i
     weight_limit = 10
     g = create_complete_weighted_undirected_graph(n,weight_limit)
-    path = [None]*(n+1)
+    config = [None]*(n)
     min_weight = None
-    min_path = None
-
-    # print(g.Vertices)
-    #print(g.Edges)
-
+    min_tour = None
     getMinTour(g,n,-1)
-    print(n,min_path,min_weight)
+    print(n,min_tour,min_weight)
